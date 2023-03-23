56f4caabd651f5fc6ada4e8d4a3c62161ef6def1
mahmoudmohamed22 <eng.mahmoud.elghonemy@gmail.com>
2023-03-23 11:49:46 +0200

    verify users by Jwttoken

---
+++ helper/verify.js
@@ -0,0 +1,40 @@
+const User=require('../models/userSchema')
+const jwt=require('jsonwebtoken');
+const {promisify}=require('util')
+const verifyJwt=promisify(jwt.verify)
+
+module.exports=async (req,res,next)=>{
+    console.log(req.headers.authorization)
+//extract token from headers
+//verify the token (secret)
+//find user by id 
+//attach  user to request body
+//middleware to check if user exist and authroized
+try
+{
+const token =req.headers.authorization;
+if(!token)
+{
+    const error=new Error('unauthorized');
+    error.statusCode=401;
+    return next(error)
+
+}
+const {id}=await verifyJwt(token,"mySecret")
+const user=await User.findById(id)
+if(!user)
+{
+    const error=new Error('unauthorized');
+    error.statusCode=401;
+    return next(error)
+
+}
+req.user=user;
+next();
+}catch(err)
+{
+console.log(err)
+next(err)
+
+}
+}
