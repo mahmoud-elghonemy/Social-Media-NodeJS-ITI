e680d38fefb5d5bd1c895d1c4aec20aade2013f4
mahmoudmohamed22 <eng.mahmoud.elghonemy@gmail.com>
2023-03-23 10:44:53 +0200

    handle global error

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express= require('express')
 const app=express()
+
 require('./db')
+
+
+
+
+
+
+// 4 parameters error handler
+app.use((err,req,res,next)=>{
+	err.statusCode = err.statusCode || 500;
+	console.log('from error handler');
+	res.status(err.statusCode).json({
+		status: 'error',
+		message:err.message || 'something went wrong',
+		err
+	})
+});
+
 
 
 app.listen(8000,()=>{
