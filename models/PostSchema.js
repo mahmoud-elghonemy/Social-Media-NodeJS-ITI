70e38acd5d47722950f710e8cfea3106b114b138
mahmoudmohamed22 <eng.mahmoud.elghonemy@gmail.com>
2023-03-23 13:33:04 +0200

    create post schema

---
+++ models/PostSchema.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const User = require("./UserSchema");
+
+
+
+const PostSchema = new Schema({
+    post: {
+        type: String,
+        required: true
+    } ,
+    User: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    }
+})
+
+const Post = mongoose.model('Post', PostSchema);
+module.exports= Post;
