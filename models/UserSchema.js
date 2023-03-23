5cd22536b52e7487f0db8c713344abc0ea082bc1
mahmoudmohamed22 <eng.mahmoud.elghonemy@gmail.com>
2023-03-23 12:01:35 +0200

    add hasing password in userschema

--- models/UserSchema.js
+++ models/UserSchema.js
@@ -32,5 +32,19 @@ const UserSchema = mongoose.Schema({
     }
    
   });
-
+  
+UserSchema.pre('save',async function(next){
+	const userDocument = this;
+	if(userDocument.isModified('password')){
+		const hashedPassword = await bcrypt.hash(userDocument.password,saltRound);
+		userDocument.password = hashedPassword;
+	}
+	next();
+})
+UserSchema.methods.comparePassword = function (password){
+	const userDocument = this;
+	return bcrypt.compare(password,userDocument.password)
+}
+// userSchema.statics.
+const User = mongoose.model('User',UserSchema);
 module.exports = mongoose.model('User', UserSchema);
