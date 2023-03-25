9af884ff142f18560c566a6a61bc42c15143ea67
mahmoudmohamed22 <eng.mahmoud.elghonemy@gmail.com>
2023-03-25 16:51:16 +0200

    fix issue to check email

--- helper/validator.js
+++ helper/validator.js
@@ -3,9 +3,9 @@ const Joi = require('joi');
 // Login validation
 const loginSchema = Joi.object({
     email: Joi.string()
-        .alphanum()
-        .required(),
-
+    .required()
+    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }), 
+    
     password: Joi.string()
         .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
     })
