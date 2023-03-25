cae1a41b5608b2d95e88fc3b107111c25f689b9d
mahmoudmohamed22 <eng.mahmoud.elghonemy@gmail.com>
2023-03-25 18:04:48 +0200

    verfiy creator to create post

--- routes/PostRoute.js
+++ routes/PostRoute.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const Post = require('../models/PostSchema');
-
+const verify = require('../helper/verify');
 //get all posts 
-router.get('/',async (req,res,next)=>{
+router.get('/',verify.authorizedCreator,async (req,res,next)=>{
     posts=await Post.find({});
     res.send(posts);
 })
 
 
 //get one post
-router.get('/:id',async (req,res,next)=>{
+router.get('/:id',verify.authorizedCreator,async (req,res,next)=>{
     oldpost=await Post.findById(req.params.id);
     res.send(oldpost);
 })
 
 //create post 
-router.post('/',async (req,res,next)=>{
+router.post('/',verify.authorizedCreator,async (req,res,next)=>{
     const post = await Post.create({
 		post:req.body.post
 		// user: req.user._id
@@ -26,7 +26,7 @@ router.post('/',async (req,res,next)=>{
 })
 
 //edit  post 
-router.patch('/:id',async (req,res,next)=>{
+router.patch('/:id',verify.authorizedCreator,async (req,res,next)=>{
     const updatepost=req.body.post;
     const upPost =await Post.findByIdAndUpdate(req.params.id,{post:updatepost},{new: true});
     res.send(upPost);
@@ -35,14 +35,14 @@ router.patch('/:id',async (req,res,next)=>{
 })
 
 //delete this post
-router.delete('/:id',async (req,res,next)=>{
+router.delete('/:id',verify.authorizedCreator,async (req,res,next)=>{
     await Post.findByIdAndDelete(req.params.id,{new: true});
     res.send("post is deleted");
 
 })
 
 //delete all posts 
-router.delete('/',async (req,res,next)=>{
+router.delete('/',verify.authorizedCreator,async (req,res,next)=>{
 
     await Post.deleteMany({});
     res.send("All posts are deleted")
