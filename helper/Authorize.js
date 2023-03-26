811db0b430f281ff2fcd04c02ab8caf7da177eb1
MahmoudMansourr <Mahmoud.M.Mansourr@gmail.com>
2023-03-26 17:40:07 +0200

    "not yet functional"

--- helper/Authorize.js
+++ helper/Authorize.js
@@ -1,8 +1,23 @@
 const Comment = require('../models/CommentSchema');
 const User = require('../models/UserSchema');
 //const Post = require('../models/PostSchema');
+const jwt=require('jsonwebtoken');
+const {promisify}=require('util')
+const verifyJwt=promisify(jwt.verify)
 
 const authorizedUser = async (req,res,next)=>{
+
+    const token =req.headers.authorization;
+    if(!token)
+    {
+        const error=new Error('unauthorized');
+        error.statusCode=401;
+        return next(error)
+
+    }
+const {id}=await verifyJwt(token,"mySecret")
+const user=await User.findById(id)
+
     const comment =  await Comment.find({_id: req.params.comment_id});
     // return error if the comment is not found
     if(!comment){
@@ -12,12 +27,12 @@ const authorizedUser = async (req,res,next)=>{
     }
 
     // autherize if the user is an admin or the comment creator
-    if(req.user.role == "admin" || comment.user == req.user._id){
+    if(user.role == "admin" || comment.user == id){
         return next();  
     }
 
     // return error if it's another user
-    if(comment.user != req.user._id){
+    if(comment.user != id){
         const error = new Error("Not authorized to do this action");
         error.statusCode = 403;
         return next(error);
