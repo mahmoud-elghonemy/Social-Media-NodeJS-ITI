ead432c5da7da93f4edfa55ca97a87493eb979c2
mahmoudmohamed22 <eng.mahmoud.elghonemy@gmail.com>
2023-03-27 06:53:24 +0200

    inital cloudinary multer functions

---
+++ cloudinary.js
@@ -0,0 +1,12 @@
+const cloudinary=require('cloudinary')
+
+const dotenv=require('dotenv')
+
+dotenv.config()
+
+cloudinary.config({
+
+    cloud_name:process.env.CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET
+})
