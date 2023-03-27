693ee97524af98554b4b2ceb3efa41bf70be309a
mahmoudmohamed22 <eng.mahmoud.elghonemy@gmail.com>
2023-03-27 08:25:20 +0200

    add put endpoint and add pic in signup

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -9,11 +9,25 @@ const verify = require('../helper/verify');
 const CustomError = require('../helper/customError');
 const router=express.Router()
 const {validateSignin}=require('../helper/validator')
+const cloudinary=require('../cloudinary')
+const upload=require('../multer')
+
+router.post('/',upload.single("image"),async(req,res)=>{
+
+   try {
+      const result=await cloudinary.uploader.upload(req.file.path)
+      res.json(result);
+   }catch(err){
+    console.log(err)
+   }
+
+})
 
 
-router.post('/signup',async (req,res,next)=>{
+router.post('/signup',upload.single("profile_pic"),async (req,res,next)=>{
     const {firstname,lastname,username,email,age,password,role }=req.body;
-   
+    const result=await cloudinary.uploader.upload(req.file.path)
+
     try{
          const newUser = new User({
              firstname,
@@ -22,7 +36,9 @@ router.post('/signup',async (req,res,next)=>{
              email,
              age,
              password,
-             role
+             role,
+             profile_pic:result.secure_url,
+             cloudinary_id:result.public_id
          });
          await newUser.save();
          res.send(newUser);
@@ -76,19 +92,36 @@ router.get('/:id',verify.authorizedAdmin,async (req,res,next)=>{
 })
 
 
-//update attribute in user 
-router.patch('/:id',verify.authorizedAdmin,async(req,res,next)=>{
-
-})
 //updates all attributes for user 
 router.put('/:id',verify.authorizedAdmin,async(req,res,next)=>{
+   let user=await User.findById(req.params.id);
+   await cloudinary.uploader.destroy(user.cloudinary_id);
+
+   const result=await cloudinary.uploader.upload(req.file.path);
+   const data={
+    firstname:req.body.firstname || user.firstname,
+    lastname:req.body.lastname || user.lastname,
+    username:req.body.username || user.username,
+    email:req.body.email || user.email,
+    age:req.body.age || user.age,
+    password:req.body.password || user.password,
+    role:req.body.role || user.role,
+    profile_pic:result.secure_url || user.profile_pic ,
+    cloudinary_id:result.public_id || user.cloudinary_id
+   }
+   user=await User.findByIdAndUpdate(req.params.id,data,{new:true});
+   res.send('the User is updated')
+
 
 })
 
 // delete user
 //remain check admin or no-->authorize this  
 router.delete('/:id',verify.authorizedAdmin,async (req,res,next )=>{
-    await User.findByIdAndDelete(req.params.id,{new: true});
+    
+    let user=await User.findById(req.params.id);
+    await cloudinary.uploader.destroy(user.cloudinary_id);
+    await user.remove();
     res.send("User is deleted");
 
 })
